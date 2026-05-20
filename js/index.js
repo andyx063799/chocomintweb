0bac5a4c1442119a22142a268f7dc0018f42e22b
Andyydev <146801090+andyx063799@users.noreply.github.com>
2026-05-19 21:26:38 -0600

    v1.3
    - Rediseño sutil de la estructura
    - Se añadio los colaboradores
    - Se añadio el efecto de glassmorphism

--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
 function switchTab(tab) {
-    // Elementos de vistas
     const botView = document.getElementById('bot-view');
     const comunidadView = document.getElementById('comunidad-view');
-    // Botones
     const btnBot = document.getElementById('btn-bot');
     const btnComunidad = document.getElementById('btn-comunidad');
-    // Enlace del menú
     const navFeatures = document.getElementById('nav-features-link');
 
     if (tab === 'bot') {
@@ -13,14 +10,14 @@ function switchTab(tab) {
         comunidadView.classList.remove('active');
         btnBot.classList.add('active');
         btnComunidad.classList.remove('active');
-        navFeatures.style.visibility = 'visible'; // Muestra características
+        navFeatures.style.visibility = 'visible'; 
     } else {
         comunidadView.classList.add('active');
         botView.classList.remove('active');
         btnComunidad.classList.add('active');
         btnBot.classList.remove('active');
-        navFeatures.style.visibility = 'hidden'; // Oculta características en comunidad
+        navFeatures.style.visibility = 'hidden'; 
     }
-    // Resetear scroll al inicio al cambiar
+    
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
